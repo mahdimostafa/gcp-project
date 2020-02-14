5a51d64dfda337a180f3d2176fc864a1a428ec3b
mahdimostafa <mahdi.ostafa.2012@gmail.com>
2020-02-14 00:08:49 +0000

    daily dag

---
+++ dag/daily_load.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import airflow
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.models import Variable
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+
+
+
+"""
+default args to set the schedule
+"""
+
+default_args = {
+    'owner': 'refresh scripts',
+    'depends_on_past': False,
+    'on_failure_callback': None,
+    'start_date': '2019-11-19',
+    'catchup': False,
+    'retries': 0,
+}
+
+dag = DAG('data_mart_refresh_daily',
+          default_args=default_args,
+          description='Load in daily Python Script and refresh DBT models',
+          schedule_interval="0 9 * * *",
+          catchup=False
+
+          )
+
+t0 = BashOperator(
+    task_id='python_load',
+    bash_command='python3 dubai_stream.py',
+    dag=dag
+)
+
+t1 = BashOperator(
+    task_id='dbt_run',
+    bash_command='dbt run --profile prod-gcp --models tag:daily',
+    dag=dag
+)
+
+t0 >> t1
